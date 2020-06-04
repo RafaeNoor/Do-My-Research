a27f41bcc3b72fcb214fb3ba33aa8bdf2e80500d
Rafae Noor <abdurrafae98@live.com>
2020-06-05 03:51:07 +0500

    adding flash setup

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {LinkContainer } from 'react-router-bootstrap';
+import Container from "react-bootstrap/Container";
+
+
+class AboutPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <h1>About Page!</h1>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
